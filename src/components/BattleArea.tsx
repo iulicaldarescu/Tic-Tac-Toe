0af583dcb682e879e08e1cba760afc3f85f0006e
Caldarescu Iulian <caldarescuiulian@gmail.com>
2024-01-03 23:40:11 +0200

    logic done

--- src/components/BattleArea.tsx
+++ src/components/BattleArea.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import iconX from "../assets/icon-x.svg";
 import iconO from "../assets/icon-o.svg";
 
@@ -16,27 +16,85 @@ function BattleArea({
     return "X";
   };
 
-  const handleClick = (index: number) => {
+  const [mainArray, setMainArray] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  // const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleClick = (param: number) => {
     setTurn(playerTurn(turn));
-    console.log(turn);
-    console.log(index);
+
+    const newArr = mainArray.map((item, index) => {
+      if (index === param) {
+        if (turn === "X") {
+          item = iconX;
+        } else {
+          item = iconO;
+        }
+      }
+      return item;
+    });
+
+    setMainArray(newArr);
   };
+
+  // useEffect(() => {
+  //   if (
+  //     (mainArray[0] && mainArray[1] && mainArray[2]) === iconO ||
+  //     (mainArray[0] && mainArray[1] && mainArray[2]) === iconX
+  //   ) {
+  //     console.log("muie la neamuri");
+  //   }
+  // }, [mainArray]);
+
+  useEffect(() => {
+    const lines = [
+      [mainArray[0], mainArray[1], mainArray[2]],
+      [mainArray[3], mainArray[4], mainArray[5]],
+      [mainArray[6], mainArray[7], mainArray[8]],
+      [mainArray[0], mainArray[3], mainArray[6]],
+      [mainArray[1], mainArray[4], mainArray[7]],
+      [mainArray[2], mainArray[5], mainArray[8]],
+      [mainArray[0], mainArray[4], mainArray[8]],
+      [mainArray[2], mainArray[4], mainArray[6]],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (a === b && a === c) {
+        if (a === iconX) {
+          console.log("esti un mare putoi");
+        } else if (a === iconO) {
+          console.log("esti un mare puloi");
+        }
+      }
+    }
+  }, [mainArray]);
 
   return (
     <div className="md:w-2/3 lg:w-1/2 m-auto">
       {/* 9 squares */}
       <div className="pt-10">
         <div className="grid grid-cols-3 gap-8 place-items-center ">
-          {new Array(9).fill(null).map((_, index) => {
+          {mainArray.map((item, index) => {
             return (
               <div
-                onClick={() => handleClick(index)}
+                onClick={!item ? () => handleClick(index) : undefined}
                 className=" flex items-center justify-center py-5 rounded-lg
-                 bg-[#1f3540] border-[#102129] custom-shadow w-[100px]"
+                 bg-[#1f3540] border-[#102129] custom-shadow w-[100px] h-[95px]"
                 key={index}
               >
                 {" "}
-                <img src={`${index ? iconX : iconO}`}></img>
+                <img src={item} alt=""></img>
               </div>
             );
           })}
